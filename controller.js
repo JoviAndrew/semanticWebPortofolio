5825e0be45314a01af50c26467133a3ceed20884
JoviAndrew <jovian_andrew@hotmail.com>
2018-04-10 07:58:24 +0700

    add getAge feature

--- controller.js
+++ controller.js
@@ -22,15 +22,55 @@ function nextAlphabet(){
     }
 }
 
+function getAge(){
+    $('.ageDiv').hide();
+
+    let inputDate = new Date($('#dateInput').val())
+    let currentDate = new Date();
+
+    let age = currentDate.getFullYear() - inputDate.getFullYear();
+    let result = `${age} year(s) old`
+    
+    $('#yourAge').show();
+    $('#yourAge').html(result);
+}
+
+function returnBack(){
+    $('#btnBack').hide();
+    $('#btnAlpha').show();
+    $('#btnNum').show();
+    $('#btnAge').show();
+    $('#number').hide();
+    $('#alphabet').hide();
+    $('.ageDiv').hide();
+    $('#text').show();
+    $('#yourAge').hide();
+}
+
 function showNumber(){
+    $('#btnBack').show();
     $('#btnAlpha').hide();
     $('#btnNum').hide();
+    $('#btnAge').hide();
     $('#number').show();
     $('#text').hide();
 }
+
+function showAge(){
+    $('#btnBack').show();
+    $('#btnAlpha').hide();
+    $('#btnNum').hide();
+    $('#btnAge').hide();
+    $('.ageDiv').show();
+    $('#text').hide();
+    $('#yourAge').hide();
+}
+
 function showAlphabet(){
+    $('#btnBack').show();
     $('#btnNum').hide();
     $('#btnAlpha').hide();
+    $('#btnAge').hide();
     $('#alphabet').show();
     $('#text').hide();
 }
